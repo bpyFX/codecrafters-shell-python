7122959c4c43fcc45c70a520776307f006d8a686
boldyfx <boldyfx@protonmail.com>
2024-12-04 16:43:31 -0600

    test run one

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
-
+    user_input = input()
+    print(f"{user_input}: command not found")
 
 if __name__ == "__main__":
     main()
