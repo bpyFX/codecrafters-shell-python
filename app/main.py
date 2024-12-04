caac63f8db30705853a092e9a7432506a5acdc52
boldyfx <boldyfx@protonmail.com>
2024-12-04 16:52:03 -0600

    REPL added

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-
     # Wait for user input
     user_input = input()
     print(f"{user_input}: command not found")
+    #loop
+    main()
 
 if __name__ == "__main__":
     main()
