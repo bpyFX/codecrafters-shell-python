36d492f02a7c3ab2bd1f8c796d0f31649a92044e
boldyfx <boldyfx@protonmail.com>
2024-12-05 16:33:57 -0600

    echo command

--- app/main.py
+++ app/main.py
@@ -6,12 +6,15 @@ def main():
     sys.stdout.write("$ ")
     # Wait for user input
     user_input = input()
+
     #loop
     if user_input == "exit 0":
         sys.exit(0)
+    elif user_input.startswith("echo "):
+            print(user_input[5:])
     else:
         print(f"{user_input}: command not found")
-        main()
+    main()
 
 if __name__ == "__main__":
     main()
