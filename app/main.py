7199154bbeecfb7c0ebb1d15014ac2cf5dc5a48c
boldyfx <boldyfx@protonmail.com>
2024-12-06 16:32:05 -0600

    added type builtins, made code more effecient

--- app/main.py
+++ app/main.py
@@ -2,19 +2,28 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    # Wait for user input
-    user_input = input()
+    #Used for type commands that are builtin
+    valid_commands = ["echo", "type", "exit"]
 
-    #loop
-    if user_input == "exit 0":
-        sys.exit(0)
-    elif user_input.startswith("echo "):
+    while True:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
+        # Wait for user input
+        user_input = input()
+
+        #loop
+        if user_input == "exit 0":
+            sys.exit(0)
+        elif user_input.startswith("echo "):
             print(user_input[5:])
-    else:
-        print(f"{user_input}: command not found")
-    main()
+        elif user_input.startswith("type "):
+            command = user_input[5:]
+            if command in valid_commands:
+                print(f"{command} is a shell builtin")
+            else:
+                print(f"{user_input[5:]}: command not found")
+        else:
+            print(f"{user_input}: command not found")
 
 if __name__ == "__main__":
     main()
