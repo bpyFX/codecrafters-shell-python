22fae0431f81f058be4167322aeee42fa5e7566d
boldyfx <boldyfx@protonmail.com>
2024-12-07 19:35:09 -0600

    executable files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 import sys
+import os
+from sys import executable
 
 
 def main():
+
+    # Get the current PATH environment variable set during execution
+    path_dirs = os.getenv('PATH').split(os.pathsep)
+
     #Used for type commands that are builtin
     valid_commands = ["echo", "type", "exit"]
 
@@ -11,19 +17,47 @@ def main():
         # Wait for user input
         user_input = input()
 
+        # Split the input into command and arguments
+        parts = user_input.split()
+        if not parts:
+            continue
+
+
+        command = parts[0]
+        args = parts [1:]
+
         #loop
         if user_input == "exit 0":
             sys.exit(0)
-        elif user_input.startswith("echo "):
-            print(user_input[5:])
-        elif user_input.startswith("type "):
-            command = user_input[5:]
-            if command in valid_commands:
-                print(f"{command} is a shell builtin")
+        elif command == "echo":
+            print(" ".join(args))
+        elif command == "type":
+            if args:
+                search_command = args[0]
+                if search_command in valid_commands:
+                    print(f"{search_command} is a shell builtin")
+                else:
+                    found = False
+                    for dir in path_dirs:
+                        executable = os.path.join(dir, search_command)
+                        if os.path.isfile(executable) and os.access(executable, os.X_OK):
+                            print(f"{search_command} is {executable}")
+                            found = True
+                            break
+                    if not found:
+                        print(f"{search_command}: command not found")
             else:
-                print(f"{user_input[5:]}: not found")
+                print("Usage: type <command>")
         else:
-            print(f"{user_input}: command not found")
+            found = False
+            for dir in path_dirs:
+                executable = os.path.join(dir, command)
+                if os.path.isfile(executable) and os.access(executable, os.X_OK):
+                    os.system(user_input)
+                    found = True
+                    break
+            if not found:
+                print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
