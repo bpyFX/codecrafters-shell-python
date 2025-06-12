fbf529a6f0c82008ab7611d3d0a3e1b72f676cb4
bpyFX <parra.bryan4@gmail.com>
2025-06-12 17:13:20 -0500

    Remove CodeCrafters references

--- your_program.sh
+++ your_program.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 #
-# Use this script to run your program LOCALLY.
+# Use this script to run the shell locally.
 #
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-#
-# Learn more: https://codecrafters.io/program-interface
+set -e  # Exit early if any commands fail
 
-set -e # Exit early if any commands fail
-
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
-exec pipenv run python3 -u -m app.main "$@"
-
+exec python3 -u -m app.main "$@"
